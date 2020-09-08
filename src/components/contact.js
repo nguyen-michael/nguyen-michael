b203400f17e527fa90791946f0a15f04640f5a12
unknown <nguyen.michl@gmail.com>
2020-09-08 11:11:29 -0500

    Reword CTA

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
   return (
     <section className="section index-contact">
       <div className="container has-text-centered">
-        <h1 className="is-size-2 has-text-white my-6">Let me know how I can serve you.</h1>
+        <h1 className="is-size-2 has-text-white my-6">Let's collaborate:</h1>
         <div className="pb-6 my-6 has-text-centered">
           <Link to="/contact" className="button is-white is-outlined">
             Get In Touch
