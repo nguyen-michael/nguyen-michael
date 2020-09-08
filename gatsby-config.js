032ac6cf4c121ee7df67241c0fb26b06afc1a1e0
unknown <nguyen.michl@gmail.com>
2020-09-07 21:30:10 -0500

    Self host fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -97,6 +97,7 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+        omitGoogleFont: true,
       },
     },
   ],
