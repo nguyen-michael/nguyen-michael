34e2ef10c861074850f95b22145b846b6458dd8c
unknown <nguyen.michl@gmail.com>
2020-09-08 11:42:45 -0500

    Add about description

--- src/components/about.js
+++ src/components/about.js
@@ -24,28 +24,28 @@ export default function About() {
   return (
     <section id="about" className="section">
       <div className="container">
-        <div className="columns">
-          <p className="column mr-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-            omnis atque eum unde maxime, nisi saepe ex, ab fugiat facere quod
-            inventore molestias quam quis tempora odio, accusamus minima
-            sapiente? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit omnis atque eum unde maxime, nisi saepe ex, ab fugiat
-            facere quod inventore molestias quam quis tempora odio, accusamus
-            minima sapiente? Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Suscipit omnis atque eum unde maxime, nisi saepe ex, ab fugiat
-            facere quod inventore molestias quam quis tempora odio, accusamus
-            minima sapiente? Lorem ipsum dolor sit amet consectetur adipisicing
-            elit. Suscipit omnis atque eum unde maxime, nisi saepe ex, ab fugiat
-            facere quod inventore molestias quam quis tempora odio, accusamus
-            minima sapiente?
-          </p>
-          <Img
-            className="column is-one-third"
-            fluid={aboutImage.childImageSharp.fluid}
-            alt="Jumbotron"
-            style={{ borderRadius: `50%` }}
-          />
+        <div className="columns is-8 is-variable">
+          <div className="column">
+            <p className="is-size-4 has-text-weight-bold mb-2">
+              Hi, I'm Michael Nguyen, a Web Developer and Designer. I am a
+              problem-solver and solution-seeker for your small business'
+              website and online presence needs.
+            </p>
+            <p>
+              Small businesses have goals and needs that are distinct from large
+              corporations. I am happy to identify trouble patches your business
+              may be experiencing on the web, whether deployment issues,
+              customer engagement or slow-loading websites and work with you to
+              come up with solutions to get you set for the future.
+            </p>
+          </div>
+          <div className="column is-one-third">
+            <Img
+              fluid={aboutImage.childImageSharp.fluid}
+              alt="Jumbotron"
+              style={{ borderRadius: `50%` }}
+            />
+          </div>
         </div>
       </div>
     </section>
